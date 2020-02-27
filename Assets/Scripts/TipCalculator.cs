a4e10371d5c6fdc193389fcf0b94219fa6b1f3d9
eric felts <echristianfelts@gmail.com>
2020-02-27 13:20:53 -0800

    First commit.  This is the 4th challenge.

---
+++ Assets/Scripts/TipCalculator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipCalculator : MonoBehaviour
+{
+
+    public float mealcost = 20.00f;
+    public float tipAmount = 18.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TipGenereator();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+
+        TipGenereator();
+         }
+
+    }
+
+    void TipGenereator()
+    {
+        float tip_ = mealcost * tipAmount * 0.01f;
+        float mealcost_ = mealcost + tip_;
+        float tip_15 = mealcost * 0.15f;
+        float mealcost_15 = mealcost + tip_15;
+        float tip_20 = mealcost * 0.20f;
+        float mealcost_20 = mealcost + tip_20;
+        float tip_25 = mealcost * 0.25f;
+        float mealcost_25 = mealcost + tip_25;
+
+
+        Debug.Log("Your bill is: $" + mealcost + ".");
+        Debug.Log("A 15% tip is: $" + tip_15 + " and brings the cost to $" + mealcost_15 + ".");
+        Debug.Log("A 20% tip is: $" + tip_20 + " and brings the meal cost to $" + mealcost_20 + ".");
+        Debug.Log("A 25% tip is: $" + tip_25 + " and brings the meal cost to $" + mealcost_25 + ".");
+        Debug.Log("And your custom " + tipAmount + "% tip is: " + tip_ + ".  That would be a total bill of : $" + mealcost_ + ".");
+
+    }
+}
---
+++ Assets/Scripts/_04_QuizScript.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _04_QuizScript : MonoBehaviour
+{
+    public float quiz1 = 0f;
+    public float quiz2 = 0f;
+    public float quiz3 = 0f;
+    public float quiz4 = 0f;
+    public float quiz5 = 0f;
+    private float _quizTotal;
+    private float _quizAvge;
+    private int _switchValue;
+
+    private string _letterGrade = "X";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuizReset();
+        _quizAvge = Mathf.RoundToInt((quiz1 + quiz2 + quiz3 + quiz4 + quiz5) / 5);
+        SwitchValueCalculator();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            QuizReset();
+            _quizAvge = Mathf.RoundToInt((quiz1 + quiz2 + quiz3 + quiz4 + quiz5) / 5);
+            //_quizAvge = Mathf.Round(_quizAvge * 100f) / 100f;
+            SwitchValueCalculator();
+            GradetoLetterConverter();
+
+            Debug.Log(" The avarage of your grades is "+ _quizAvge + ".  That means your letter Grade is an: "+ _letterGrade+".");
+        }
+
+    }
+
+    void QuizReset()
+    {
+        quiz1 = Mathf.Round(Random.Range(50f, 100f) * 100f) / 100f;
+
+        quiz2 = Mathf.Round(Random.Range(50f, 100f) * 100f) / 100f;
+        quiz3 = Mathf.Round(Random.Range(50f, 100f) * 100f) / 100f;
+        quiz4 = Mathf.Round(Random.Range(50f, 100f) * 100f) / 100f;
+        quiz5 = Mathf.Round(Random.Range(50f, 100f) * 100f) / 100f;
+
+    }
+
+    void SwitchValueCalculator()
+    {
+        _switchValue = 0;
+
+        if (_quizAvge <90)
+        {
+            _switchValue = 1;
+
+            if (_quizAvge < 80)
+            {
+                _switchValue = 2;
+
+                if (_quizAvge < 70)
+                {
+                    _switchValue = 3;
+                    if (_quizAvge < 60)
+                    {
+                        _switchValue = 4;
+                    }
+                }
+            }
+        }
+
+    }
+
+    void GradetoLetterConverter()
+    {
+        switch (_switchValue)
+        {
+            case 0: //easy
+                _letterGrade = "A";
+                Debug.Log("Easy..!");
+                break;
+            case 1: //medium
+                _letterGrade = "B";
+                Debug.Log("OK..!");
+                break;
+            case 2: //hard
+                _letterGrade = "C";
+                Debug.Log("Achevable..!");
+                break;
+            case 3: //hard
+                _letterGrade = "D";
+                Debug.Log("Too Hard..!");
+                break;
+            case 4: //hard
+                _letterGrade = "F";
+                Debug.Log("Way Too Hard..!");
+                break;
+            default:
+                Debug.Log("Invalid Selection..!!");
+                break;
+
+        }
+
+    }
+
+
+}
