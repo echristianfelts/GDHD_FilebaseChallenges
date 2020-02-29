44ecdc0c7ea0fc5a52776c0f7aaf50f67d9c24ba
eric felts <echristianfelts@gmail.com>
2020-02-28 17:26:15 -0800

    Starting Filebase Challenge 11.

---
+++ Assets/Scripts/_10_tellMeNames.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _10_tellMeNames : MonoBehaviour
+{
+
+    public string[] nameArray = new string[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (string name in nameArray)
+        {
+            Debug.Log(name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
