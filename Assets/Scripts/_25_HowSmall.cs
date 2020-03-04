571107fed12593aacc24f8f84b328ffe8aed47bd
eric felts <echristianfelts@gmail.com>
2020-03-04 13:38:47 -0800

    Finished Filebase Challenge 26.

---
+++ Assets/Scripts/_25_HowSmall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _25_HowSmall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.transform.localScale = this.transform.localScale * .5f;
+        }
+    }
+}
---
+++ Assets/Scripts/_26_MagicTrick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _26_MagicTrick : MonoBehaviour
+{
+    [SerializeField]
+    private float _counterTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _counterTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            _counterTime = Time.time + 5f;
+            //this.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+            this.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f, 0f);
+        }
+
+        if (_counterTime < Time.time)
+        {
+            this.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+
+
+        }
+    }
+}
