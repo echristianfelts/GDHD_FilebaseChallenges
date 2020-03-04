f374465f1251845c389504278df6707641cfdc13
eric felts <echristianfelts@gmail.com>
2020-03-04 12:57:06 -0800

    Filebase Challenge 24: Fallign forever.  Done.

---
+++ Assets/Scripts/_24_FallingForever.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _24_FallingForever : MonoBehaviour
+{
+    float _force = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("We are getting some action...");
+        this.GetComponent<Rigidbody>().AddForce(Vector3.up * _force, ForceMode.Impulse);
+        this.transform.position = new Vector3(0f, 5f, 0f);
+        //_force = _force + _force;
+    }
+
+}
