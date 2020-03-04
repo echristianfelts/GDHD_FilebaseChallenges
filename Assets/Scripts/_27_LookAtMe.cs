f240ec0c4c10f92d2abacb1d497011bc8cdff4bf
eric felts <echristianfelts@gmail.com>
2020-03-04 14:47:18 -0800

    Finished Filebase Challenge 27.

---
+++ Assets/Scripts/_27_LookAtMe.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _27_LookAtMe : MonoBehaviour
+{
+    public GameObject _lookAtTarget;
+    private GameObject _camRoot;
+    public float _rootRotSpeed = 1f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camRoot = GameObject.Find("CameraRoot");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _camRoot.transform.Rotate(0f, 1f * _rootRotSpeed, 0f);
+        Vector3 directionToFace = _lookAtTarget.transform.position - transform.position;
+        Debug.DrawRay(transform.position, directionToFace, Color.green);
+
+        Quaternion targetRotation = Quaternion.LookRotation(directionToFace);
+
+        transform.rotation =  targetRotation;
+
+    }
+}
---
+++ Assets/Scripts/_28_RandomRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _28_RandomRotation : MonoBehaviour
+{
+    [SerializeField]
+    private float _counterTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _counterTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+            //_counterTime = Time.time + 5f;
+            //this.gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+            //this.GetComponent<Renderer>().material.color = new Color(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+
+
+        if (_counterTime < Time.time)
+        {
+            Debug.Log("Do we ever get here...?");
+            this.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+            this.transform.Rotate(0f,Random.Range(0f, 360f), 0f);
+            _counterTime = Time.time + 5f;
+        }
+    }
+}
