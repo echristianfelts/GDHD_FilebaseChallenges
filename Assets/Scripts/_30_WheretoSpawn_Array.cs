0402eb39d703cc8b7b0224162e8e86683f1855e9
eric felts <echristianfelts@gmail.com>
2020-03-04 17:12:18 -0800

    Filebase Challenge Number 31.  Finished.

---
+++ Assets/Scripts/_30_WheretoSpawn_Array.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _30_WheretoSpawn_Array : MonoBehaviour
+{
+    public GameObject[] targetBlocks = new GameObject[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            int randoNumber = Random.Range(0, 4);
+            Debug.Log("randoNumber = " + randoNumber);
+
+            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+            float randoNumberX = targetBlocks[randoNumber].transform.position.x;
+            float randoNumberZ = targetBlocks[randoNumber].transform.position.z;
+
+
+            sphere.transform.position = new Vector3(randoNumberX, 1.5f, randoNumberZ);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/_31_ChangeColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _31_ChangeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            this.GetComponent<Renderer>().material.color = Color.red;
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            this.GetComponent<Renderer>().material.color = Color.blue;
+
+        }
+    }
+}
