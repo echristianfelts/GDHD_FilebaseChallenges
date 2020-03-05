b40ff2b00816e0063e29ccc6c4155e0338605683
eric felts <echristianfelts@gmail.com>
2020-03-05 12:16:45 -0800

    Filebase Challenge 35.  Done.

---
+++ Assets/Scripts/_35_ClicktoSpawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _35_ClicktoSpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _raycastTarget;
+    [SerializeField]
+    GameObject _spawnObject;
+    int _index = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouseclick Positive");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.Log(ray);
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction);
+
+            //this if checks, a detection of hit in an GameObject with the mouse on screen
+            if (Physics.Raycast(ray, out hit) && hit.collider.tag == "floor")
+            {
+
+                GameObject newEnemy = Instantiate(_spawnObject, hit.point + new Vector3(0f,0.5f,0f), Quaternion.identity);
+                newEnemy.name = "newEnemy_" + _index.ToString();
+                _index++;
+                //      GameObject.Find("Nameofyourobject") search your gameobject on the hierarchy with the desired name and allows you to use it
+                //      Destroy(this.gameObject);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/_35_SpawnChecker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _35_SpawnChecker : MonoBehaviour
+{
+    float _localBirthTime;
+    float _age;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _localBirthTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _age = Time.time - _localBirthTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(Time.time);
+
+        if (other.tag == "avoid" && _age < .01)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log(Time.time);
+
+    //    if (collision.collider.tag == "avoid" && _age <.01)
+    //    {
+    //        Destroy(this.gameObject);
+    //    }
+    //}
+}
