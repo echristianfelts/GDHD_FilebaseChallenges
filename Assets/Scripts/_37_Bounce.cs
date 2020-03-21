150a72de356fa47e7f017850a2ecee107683a392
Chris felts <echristianfelts@gmail.com>
2020-03-21 04:33:45 -0700

    FileBase Challenges number 37

---
+++ Assets/Scripts/_37_Bounce.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _37_Bounce : MonoBehaviour
+{
+    public float bounceForce=1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("We are getting some action...");
+        this.GetComponent<Rigidbody>().AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+        
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("We have been here too long...");
+        this.GetComponent<Rigidbody>().AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/_37_FreezeTime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _37_FreezeTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.timeScale == 1.0f)
+                Time.timeScale = 0.01f;
+            else
+                Time.timeScale = 1.0f;
+            // Adjust fixed delta time according to timescale
+            // The fixed delta time will now be 0.02 frames per real-time second
+            Time.fixedDeltaTime = Time.fixedDeltaTime * Time.timeScale;
+        }
+    }
+}
